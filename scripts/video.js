6c764db835618680ba4475b6cf8ab3b52b69d53d
atmabdullaham <atmabdullaham@gmail.com>
2025-01-14 20:19:49 +0600

    Load And Display Videos Based On Category

--- scripts/video.js
+++ scripts/video.js
@@ -1,23 +1,24 @@
 // _________load categories
 const loadCategories = () => {
- // __Fetch the data
- fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-  .then(res => res.json())
-  .then(data => displayCategories(data.categories))
-  .catch((error) => console.log(error));
+  // __Fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then(res => res.json())
+    .then(data => displayCategories(data.categories))
+    .catch((error) => console.log(error));
 }
 
 //_________display categories
 const displayCategories = (categories) => {
- const categoryContainer = document.getElementById("categories");
- categories.forEach(item => {
-  // create a button
-  const button = document.createElement("button");
-  button.classList = "btn";
-  button.innerText = item.category;
-  //  add button to the category container
-  categoryContainer.append(button);
- });
+  const categoryContainer = document.getElementById("categories");
+  categories.forEach(item => {
+    // create a button
+    const buttonContainer = document.createElement("div");
+    buttonContainer.innerHTML = `
+    <button onclick = "loadCategoryVideo(${item.category_id})" class = "btn" >${item.category} </button>
+    `
+    //  add button to the category container
+    categoryContainer.append(buttonContainer);
+  });
 
 }
 loadCategories();
@@ -27,27 +28,28 @@ loadCategories();
 
 // ________load videos
 const loadVideos = () => {
- // __Fetch the data
- fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
-  .then(res => res.json())
-  .then(data => displayVideos(data.videos))
-  .catch((error) => console.log(error));
+  // __Fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then(res => res.json())
+    .then(data => displayVideos(data.videos))
+    .catch((error) => console.log(error));
 }
 loadVideos();
 
 // _________display videos,
 const displayVideos = (videos) => {
- const videoContainer = document.getElementById("videos")
- videos.forEach(video => {
-  console.log(video)
+  const videoContainer = document.getElementById("videos")
+  videoContainer.innerHTML = "";
+  videos.forEach(video => {
+    console.log(video)
 
 
 
 
 
-  const card = document.createElement("div");
-  card.classList = "card card-compact"
-  card.innerHTML = `
+    const card = document.createElement("div");
+    card.classList = "card card-compact"
+    card.innerHTML = `
   
   <figure class = "h-[200px]">
     <img
@@ -71,26 +73,35 @@ ${video.authors[0].verified == true ? '<img id = "verification-status" class="w-
     </div >
   </div >
  `
-  videoContainer.append(card);
- });
+    videoContainer.append(card);
+  });
 }
 
+
+const loadCategoryVideo = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then(res => res.json())
+    .then(data => displayVideos(data.category))
+    .catch((error) => console.log(error));
+}
+
+
 const videos = [
- {
-  "category_id": "1001",
-  "video_id": "aaaa",
-  "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-  "title": "Shape of You",
-  "authors": [
-   {
-    "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-    "profile_name": "Olivia Mitchell",
-    "verified": ""
-   }
-  ],
-  "others": {
-   "views": "100K",
-   "posted_date": "16278"
-  },
-  "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
- }]
+  {
+    "category_id": "1001",
+    "video_id": "aaaa",
+    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+    "title": "Shape of You",
+    "authors": [
+      {
+        "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+        "profile_name": "Olivia Mitchell",
+        "verified": ""
+      }
+    ],
+    "others": {
+      "views": "100K",
+      "posted_date": "16278"
+    },
+    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+  }]
