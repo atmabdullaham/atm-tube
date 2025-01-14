819a4ce51fa2bbaa2f6db48ead8abe10a8a28edc
atmabdullaham <atmabdullaham@gmail.com>
2025-01-14 20:36:55 +0600

    Show Conditional No Content Text For Empty Array

--- scripts/video.js
+++ scripts/video.js
@@ -40,6 +40,19 @@ loadVideos();
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("videos")
   videoContainer.innerHTML = "";
+
+  if (videos.length == 0) {
+    videoContainer.classList.remove("grid")
+    videoContainer.innerHTML = `
+    <div class ="min-h-[300px] flex flex-col gap-5 justify-center items-center"  > 
+       <img src ="../assets/Icon.png" />
+       <h2 class ="text-center text-xl font-bold" >No Content Here in This Category </h2>
+    </div>
+    `
+    return;
+  } else {
+    videoContainer.classList.add("grid");
+  }
   videos.forEach(video => {
     console.log(video)
 
